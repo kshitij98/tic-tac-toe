5f152a92ad55391ed7060506077099b177b9d927
Pratik Jain <pratik.jain448@gmail.com>
2018-02-19 11:45:59 +0530

    Initial Commit

---
+++ Team29.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import time as time
+import os as os
+
+config = {
+	TIME_LIM: 14
+}
+
+'''
+	Node will contain information regarding a state of Board.
+	Other information like evaluation function, etc.
+	We need to figure out way to distinguish between a small
+	and a big board.
+'''
+class Node:
+	pass
+
+
+'''
+	This will contain the information just reagarding the Board
+	state. 'x' denotes First player and 'o' denotes second player
+'''
+
+class Board:
+	pass
