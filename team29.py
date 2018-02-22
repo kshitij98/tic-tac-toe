427496ff91b188f90a744907a9aeaf15f798cd79
Pratik Jain <pratik.jain0509@gmail.com>
2018-02-22 13:50:12 +0530

    Updated tesm29.py

--- team29.py
+++ team29.py
@@ -3,7 +3,7 @@ import time as time
 import os as os
 
 config = {
-	TIME_LIM: 14
+	TIME_LIM: 15
 }
 
 '''
@@ -11,15 +11,19 @@ config = {
 	Other information like evaluation function, etc.
 	We need to figure out way to distinguish between a small
 	and a big board.
-'''
-class Node:
-	pass
-
-
-'''
 	This will contain the information just reagarding the Board
 	state. 'x' denotes First player and 'o' denotes second player
 '''
 
-class Board:
-	pass
+class Team29:
+
+	'''
+	This will return the move to be played
+	@board: OBJECT
+		board_status: String containing X, - or O for whole Board
+		block_status: String containing X, - or O for the Block
+	@old_move: (X, Y); initially (-1, -1)
+	@flag: Marker X or O
+	'''
+	def move(self, board, old_move, flag):
+		pass
