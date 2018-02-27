73ad1074d70728b4206a4068892f7a56b03b8755
kshitij98 <kgupta119@gmail.com>
2018-02-27 19:59:50 +0530

    Updated BOT

--- Opponent/kshitij.py
+++ Opponent/kshitij.py
@@ -26,6 +26,7 @@ class Kshitij:
 	def __init__(self):
 		# Set BONUS_MOVE = false
 		self.player = 'P1'
+		self.opponent = 'P2'
 		self.isBonusMove = False
 		self.pivot = [None] * 2
 		self.board = [[None for i in range(16)] for j in range(16)]
@@ -59,63 +60,159 @@ class Kshitij:
 			return True
 		return False
 
+
+	# TODO: Assign exponential probabilities?
+	def getWinProb(self, filled):
+		if filled < 0:
+			return 0
+		return 2 ^ filled
+
+
+				# 0, 0
+	# 1, 0					1, 1
+				# 0, 1
+	def getDiamondChance(self, offset, player):
+		chance = 0
+		for i in xrange(2):
+			for j in xrange(2):
+				x, y = offset[0] + 1 - i + 2*j*i, offset[1] + i + (1-i)*j*2
+				if self.board[x][y] == config[player]:
+					chance += 1
+				elif self.board[x][y] != config['EM']:
+					chance = -config['INF']
+		return chance
+
+
 	def getWinningChance(self, currBlock, player):
+		x, y = currBlock[0] * 4, currBlock[1] * 4
+
 		if self.block[currBlock[0]][currBlock[1]] != config['EM']:
-			return 
+			return 5 # TODO: Set constant value, Not infinity
+
+		chance = 0
+
+		for i in xrange(4):
+			currChance = 0
+			for j in xrange(4):
+				if self.board[x + i][y + j] == config[player]:
+					currChance += 1
+				elif self.board[x + i][y + j] != config['EM']:
+					currChance = -config['INF']
+			if currChance >= 0:
+				# TODO: Can merge with a different function
+				chance += self.getWinProb(currChance)
+
+		for i in xrange(4):
+			currChance = 0
+			for j in xrange(4):
+				if self.board[x + j][y + i] == config[player]:
+					currChance += 1
+				elif self.board[x + i][y + j] != config['EM']:
+					currChance = -config['INF']
+			if currChance >= 0:
+				chance += self.getWinProb(currChance)
+
+		for i in xrange(2):
+			for j in xrange(2):
+				chance += self.getWinProb(self.getDiamondChance((x + i, y + j), player))
+
+		return chance
+
 
 	def assignPoints(self, currBlock, points):
-		if self.isValuable(currBlock):
-			if self.isBonusMove:
-				pass
-			else:
-				pass
+		x, y = currBlock[0] * 4, currBlock[1] * 4
+		invalidBlock = self.isOpenMove(currBlock)
 
-		else:
-			pass
+		winTable = [[0 for i in range(4)] for j in range(4)]
+		oppWinTable = [[0 for i in range(4)] for j in range(4)]
+		uselessnessTable = [[0 for i in range(4)] for j in range(4)]
+
+
+		for i in xrange(4):
+			for j in xrange(4):
+				if self.board[x + i][y + j] != config['EM'] or invalidBlock:
+					points[x + i][y + j] = -config['INF']
+				else:
+					self.board[x + i][y + j] = config[self.player]
+					winTable[i][j] = self.getWinningChance(currBlock, self.player)
+					oppWinTable[i][j] = self.getWinningChance((i, j), self.opponent)
+					# TODO: Update constants
+					if self.isValuable((i, j)):
+						uselessnessTable[i][j] = 1
+					else:
+						uselessnessTable[i][j] = 5
+						
+					self.board[x + i][y + j] = config['EM']
+
+		# print(winTable)
+		# print(oppWinTable)
+		# print(uselessnessTable)
+		# print(points)
+
+		# TODO: Set constants
+		c1, c2, c3 = 1, 1, 1
+		if self.isValuable(currBlock):
+			if self.isBonusMove: # Give lesser wight to winning this block, more to useless transfer
+				c1, c2, c3 = 2, 1, 3
+			else: # Give high weight to winning this block, lesser to useless transfer
+				c1, c2, c3 = 3, 1, 2
+
+		else: # Give higher weight to winning this block, then to blocking, then to useless transfer
+			c1, c2, c3 = 6, 5, 3
+
+		for i in xrange(4):
+			for j in xrange(4):
+				if points[x + i][y + j] != -config['INF']:
+					points[x + i][y + j] = c1 * winTable[i][j] - c2 * oppWinTable[i][j] + c3 * uselessnessTable[i][j]
 
 		return points
 
-	def findBestMove(self, points, offset):
-		tableSize = len(points)
+	def findBestMove(self, points, currBlock):
 		best = (0, 0)
-		for i in xrange(tableSize):
-			for j in xrange(tableSize):
-				if points[i][j] > best:
-					best = (i, j)
+		x, y = currBlock[0] * 4, currBlock[1] * 4
+		if self.isOpenMove(currBlock):
+			for i in xrange(16):
+				for j in xrange(16):
+					if points[i][j] > points[best[0]][best[1]]:
+						best = (i, j)
+		else:
+			for i in xrange(4):
+				for j in xrange(4):
+					if points[x + i][y + j] > points[x + best[0]][y + best[1]]:
+						best = (i, j)
 
-		return (offset[0] + best[0], offset[1] + best[1])
+		return (x + best[0], y + best[1])
 
+	# def solve(self, depth, ):
+
+	# def IDF(self, ):
 
 	# Move the piece
 	def move(self, board, oldMove, flag):
 		startTime = int(time.time() * TUNIT)	# Get time in ms
 
 		if flag == config['P1']:
-			self.player = 'P1'
+			self.player, self.opponent = 'P1', 'P2'
 		else:
-			self.player = 'P2'
+			self.player, self.opponent = 'P2', 'P1'
 
 		self.updateBoardState(board.board_status, board.block_status, oldMove)
 
 		currBlock = (oldMove[0] % SZ, oldMove[1] % SZ)
 
-		pointsTable = None
+		pointsTable = [[None for i in range(16)] for j in range(16)]
 		if self.isOpenMove(currBlock):
 			currBlock = (0, 0)
-			pointsTable = [[None for i in range(16)] for j in range(16)]
-			pointsBlock = [[None for i in range(4)] for j in range(4)]
 			for i in xrange(4):
 				for j in xrange(4):
-					self.assignPoints((i, j), pointsBlock)
-					for ii in xrange(4):
-						for jj in xrange(4):
-							pointsTable[i*4 + ii][j*4 + jj] = pointsBlock[ii][jj]
+					self.assignPoints((i, j), pointsTable)
 		else:
-			pointsTable = [[None for i in range(4)] for j in range(4)]
 			self.assignPoints(currBlock, pointsTable)
 
-		foundMove = self.findBestMove(pointsTable, (currBlock[0] * 4, currBlock[1] * 4))
+		# print(pointsTable)
 
-		print(foundMove)
+		foundMove = self.findBestMove(pointsTable, currBlock)
 
-		print("Executed properly.\n")
+		# print("Executed properly.\n")
+		print(self.player)
+		return foundMove
